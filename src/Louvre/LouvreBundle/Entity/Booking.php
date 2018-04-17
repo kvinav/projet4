1ddc72f3397be4cc59a7c44cae6d35d5865df9c5
kvinav <kavignonpro@gmail.com>
2018-04-17 14:13:25 +0200

    Created Booking entity

---
+++ src/Louvre/LouvreBundle/Entity/Booking.php
@@ -0,0 +1,189 @@
+<?php
+
+namespace Louvre\LouvreBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Booking
+ *
+ * @ORM\Table(name="booking")
+ * @ORM\Entity(repositoryClass="Louvre\LouvreBundle\Repository\BookingRepository")
+ */
+class Booking
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="dateBooking", type="datetime")
+     */
+    private $dateBooking;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="dateVisit", type="datetime")
+     */
+    private $dateVisit;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="type", type="string", length=255)
+     */
+    private $type;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="email", type="string", length=255)
+     */
+    private $email;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="price", type="integer")
+     */
+    private $price;
+
+
+    /**
+     * Get id.
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set dateBooking.
+     *
+     * @param \DateTime $dateBooking
+     *
+     * @return Booking
+     */
+    public function setDateBooking($dateBooking)
+    {
+        $this->dateBooking = $dateBooking;
+
+        return $this;
+    }
+
+    /**
+     * Get dateBooking.
+     *
+     * @return \DateTime
+     */
+    public function getDateBooking()
+    {
+        return $this->dateBooking;
+    }
+
+    /**
+     * Set dateVisit.
+     *
+     * @param \DateTime $dateVisit
+     *
+     * @return Booking
+     */
+    public function setDateVisit($dateVisit)
+    {
+        $this->dateVisit = $dateVisit;
+
+        return $this;
+    }
+
+    /**
+     * Get dateVisit.
+     *
+     * @return \DateTime
+     */
+    public function getDateVisit()
+    {
+        return $this->dateVisit;
+    }
+
+    /**
+     * Set type.
+     *
+     * @param string $type
+     *
+     * @return Booking
+     */
+    public function setType($type)
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    /**
+     * Get type.
+     *
+     * @return string
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    /**
+     * Set email.
+     *
+     * @param string $email
+     *
+     * @return Booking
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get email.
+     *
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set price.
+     *
+     * @param int $price
+     *
+     * @return Booking
+     */
+    public function setPrice($price)
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    /**
+     * Get price.
+     *
+     * @return int
+     */
+    public function getPrice()
+    {
+        return $this->price;
+    }
+}
